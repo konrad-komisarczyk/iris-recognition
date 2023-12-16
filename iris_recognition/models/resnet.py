88b24b895e15f7a295e8ddb4adb9f427d86f1ae8
HKozubek <01149402@pw.edu.pl>
2023-12-16 17:01:37 +0100

    Freezing weights + new layers

--- iris_recognition/models/resnet.py
+++ iris_recognition/models/resnet.py
@@ -18,5 +18,21 @@ class Resnet(Model):
     def prepare_pretrained(self, num_classes: int) -> None:
         self.logger.debug("Loading model")
         self.model = resnet152(weights='DEFAULT')
-        self.model.fc = torch.nn.Linear(self.model.fc.in_features, num_classes)
+
+        # Freeze all the layers
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        # Replace the last fully connected layer with additional layers
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = torch.nn.Sequential(
+            torch.nn.Linear(num_ftrs, 1024),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.5),
+            torch.nn.Linear(1024, 512),
+            torch.nn.ReLU(),
+            torch.nn.Dropout(0.3),
+            torch.nn.Linear(512, num_classes)  # Final layer for 450 classes
+        )
+        
         self.logger.debug("Done loading model and preparing classification layer")
